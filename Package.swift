fa8f5aa1bc5620200b19568206557de1212887fa
Jeremy Greenwood <jeremy@mobelux.com>
2020-03-25 09:38:16 -0400

    update package name

--- Package.swift
+++ Package.swift
@@ -4,16 +4,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "Model",
+    name: "mxfitness-model",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Model",
-            targets: ["Model"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "Model", targets: ["Model"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
